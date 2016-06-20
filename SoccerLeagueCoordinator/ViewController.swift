74130e0bf2b0c0e858045b785198cf9d88171396
Abdirahman Liban <abdirahmanliban@abdirahmans-macbook-pro.local>
2016-06-20 22:25:59 +0400

    Initial Commit

---
+++ SoccerLeagueCoordinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoccerLeagueCoordinator
+//
+//  Created by Abdirahman Liban on 6/20/16.
+//  Copyright © 2016 Abdirahman Liban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
